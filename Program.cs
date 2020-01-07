6aa3e32a5515ce7080bb286c9aa00340d284e911
mahume <michael.a.hume@gmail.com>
2020-01-07 16:57:07 -0700

    Set up boilerplate code

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SkunkWorx
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      List<string> employees = new List<string>();
+
+      while (true)
+      {
+        Console.WriteLine("Please enter a name:");
+        string input = Console.ReadLine();
+        if (input == "")
+        {
+          break;
+        }
+        employees.Add(input);
+      };
+
+      for (int i = 0; i < employees.Count; i++)
+      {
+        Console.WriteLine(employees[i]);
+      };
+    }
+  }
+}
