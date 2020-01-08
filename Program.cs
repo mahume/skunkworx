42a5a8c4910b7e49f45d07f61482f7514942288e
mahume <michael.a.hume@gmail.com>
2020-01-07 18:23:14 -0700

    Extracted Printing logic to Util Class

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace SkunkWorx
     static void Main(string[] args)
     {
       List<Employee> employees = GetEmployees();
-      PrintEmployees(employees);
+      Util.PrintEmployees(employees);
     }
 
     static List<Employee> GetEmployees()
@@ -37,15 +37,6 @@ namespace SkunkWorx
       };
 
       return employees;
-    }
-
-    static void PrintEmployees(List<Employee> employees)
-    {
-      for (int i = 0; i < employees.Count; i++)
-      {
-        string template = "{0,-10}\t{1,-20}\t{2}";
-        Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetName(), employees[i].GetPhotoURL()));
-      }
     }
   }
 }
